33b320a24aa1e60d585dcc5f4f67d50c4bc3a6e9
Mickael Razakatiana <mfrazakatiana@gmail.com>
2026-06-14 13:24:50 +0200

    Correction et optimisation stockage SQLite DB.

--- src/stockage.py
+++ src/stockage.py
@@ -1,88 +1,45 @@
 # Stockage des données dans une base de données SQLite.
 
 # Imports
-
 import sqlite3
 import pandas as pd
 import os
 
+# Fonction création et connexion à la base de données.
+
+def create_database_connection():
+
+    # Chemin du répertoire pour la base de donnée.
+    db_folder_path = os.path.join("..", "data", "database")
+
+    # Vérifier et créer le répertoire de destination s'il n'existe pas.
+    if not os.path.exists(db_folder_path):
+        os.makedirs(db_folder_path)
+    
+    db_path = os.path.join("..", "data", "database", "idfm.db")
+    return sqlite3.connect(db_path)
 
 
-# 1. Création de la base de données SQLite.
+# Fonction pour créer les tables et importer les données vers la base.
 
-# Chemin du répertoire pour la base de donnée.
-db_folder_path = os.path.join("..","data","database")
+def insert_data_from_csv(csv_file, table_name, conn):
+    df = pd.read_csv(csv_file)
+    df.to_sql(table_name, conn, if_exists="replace", index=False)
 
-# Vérifier et créer le répertoire de destination s'il n'existe pas.
-if not os.path.exists(db_folder_path):
-    os.makedirs(db_folder_path)
+def main():
+    # Créer une connexion à la base de données.
+    conn = create_database_connection()
+    
+    try:
+        # Insérer les données dans les tables.
+        insert_data_from_csv(os.path.join('..', 'data', 'processed', 'stations.csv'), "stations", conn)
+        insert_data_from_csv(os.path.join('..', 'data', 'processed', 'validations.csv'), "validations", conn)
+        insert_data_from_csv(os.path.join('..', 'data', 'processed', 'validations_ligne.csv'), "validations_ligne", conn)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        # S'assurer que la connexion est fermée.
+        conn.close()
 
-db_path = os.path.join("..","data","database","idfm.db")
-
-conn = sqlite3.connect(db_path)
-
-cur = conn.cursor()
-
-
-
-# 2. Création des tables.
-
-
-# 2.1. Table des stations.
-
-# Lire le csv
-processed_stations_filepath = os.path.join('..','data','processed','stations.csv')
-stations_df = pd.read_csv(processed_stations_filepath)
-
-# Connexion à la base SQLite
-conn = sqlite3.connect(db_path)
-
-# Importer dans une table.
-stations_df.to_sql(
-    "stations",
-    conn,
-    if_exists="replace",
-    index=False
-)
-
-conn.close()
-
-
-# 2.2. Table de validations.
-
-# Lire le csv
-processed_validations_filepath = os.path.join('..','data','processed','validations.csv')
-validations_df = pd.read_csv(processed_validations_filepath)
-
-# Connexion à la base SQLite
-conn = sqlite3.connect(db_path)
-
-# Importer dans une table.
-validations_df.to_sql(
-    "validations",
-    conn,
-    if_exists="replace",
-    index=False
-)
-
-conn.close()
-
-
-# 2.3. Table de validations par ligne.
-
-# Lire le csv
-processed_validations_ligne_filepath = os.path.join('..','data','processed','validations_ligne.csv')
-validations_ligne_df = pd.read_csv(processed_validations_ligne_filepath)
-
-# Connexion à la base SQLite
-conn = sqlite3.connect(db_path)
-
-# Importer dans une table.
-validations_ligne_df.to_sql(
-    "validations_ligne",
-    conn,
-    if_exists="replace",
-    index=False
-)
-
-conn.close()
+if __name__ == "__main__":
+    main()
